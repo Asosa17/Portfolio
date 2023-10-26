4aade8776ab959d7c182071b1eff29b994f8db1e
Alejandro Sosa Fernández <alejandrosf913@gmail.com>
2023-10-26 11:48:49 +0200

    v2

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show')
         } else {
             entry.target.classList.remove('show')
@@ -11,10 +11,10 @@ const observer = new IntersectionObserver((entries) => {
 const childs = document.querySelectorAll('.child',)
 const hidden = document.querySelectorAll('.hidden',)
 
-childs.forEach((el)=> {
+childs.forEach((el) => {
     observer.observe(el)
 })
-hidden.forEach((el)=> {
+hidden.forEach((el) => {
     observer.observe(el)
 })
 
@@ -30,19 +30,44 @@ iconTheme.innerHTML = moon;
 document.body.classList.add('light-theme')
 
 function toggleTheme() {
-  if (iconTheme.innerHTML == moon) {
-    iconTheme.innerHTML = "";
-    iconTheme.innerHTML = sun;
-    document.body.classList.remove('light-theme')
-    document.body.classList.add('dark-theme')
-  } else {
-    iconTheme.innerHTML = "";
-    iconTheme.innerHTML = moon;
-    document.body.classList.remove('dark-theme')
-    document.body.classList.add('light-theme')
-  }
+    if (iconTheme.innerHTML == moon) {
+        iconTheme.innerHTML = "";
+        iconTheme.innerHTML = sun;
+        document.body.classList.remove('light-theme')
+        document.body.classList.add('dark-theme')
+    } else {
+        iconTheme.innerHTML = "";
+        iconTheme.innerHTML = moon;
+        document.body.classList.remove('dark-theme')
+        document.body.classList.add('light-theme')
+    }
 }
 
 btnTheme.addEventListener("click", () => {
     toggleTheme()
 });
+// Función para ajustar el tamaño de fuente en función del ancho de la pantalla
+function adjustFontSize() {
+    const screenWidth = window.innerWidth;
+    const text2 = document.querySelector('.text2');
+    const text3 = document.querySelector('.text3');
+
+    if (screenWidth <= 768) {
+        text2.style.fontSize = '20px';
+        text3.style.fontSize = '16px';
+    } else if (screenWidth <= 1024) {
+        text2.style.fontSize = '30px';
+        text3.style.fontSize = '24px';
+    } else {
+        text2.style.fontSize = '40px';
+        text3.style.fontSize = '32px';
+    }
+}
+
+// Llama a la función inicialmente y cada vez que cambie el tamaño de la ventana
+adjustFontSize();
+window.addEventListener('resize', adjustFontSize);
+
+
+
+
